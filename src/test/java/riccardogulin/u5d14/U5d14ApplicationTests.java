711f1d39c697de4228072f5b8c3da244555070b3
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-08-24 10:05:11 +0200

    Adds basic Spring config

---
+++ src/test/java/riccardogulin/u5d14/U5d14ApplicationTests.java
@@ -0,0 +1,13 @@
+package riccardogulin.u5d14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
